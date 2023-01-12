a11a4cff00c782d385f557fef7ec6e34482bb94d
Yaroslav <kucherenko.yaro@gmail.com>
2023-01-12 23:50:56 +0200

    port chenge

--- Game-store/client/src/configs/API.js
+++ Game-store/client/src/configs/API.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.REACT_APP_API_URL;
+export const API_URL = process.env.REACT_APREACT_APP_API_URL;
--- Game-store/server2/index.js
+++ Game-store/server2/index.js
@@ -24,7 +24,7 @@ app.use(
 	})
 );
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 // const MONGO_URL =
 // 	"mongodb+srv://admin:admin@cluster0.vqljl7l.mongodb.net/node-blog?retryWrites=true&w=majority";
@@ -48,7 +48,7 @@ const start = async () => {
 			})
 			.catch((err) => console.log("DB error", err));
 
-		app.listen(process.env.PORT || 5000, () => {
+		app.listen(PORT, () => {
 			console.log(`Server is running on :${PORT}`);
 		});
 	} catch (error) {
